84ae77daa467cdd7c31e3ddf2291ce2d020b97e0
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2022-02-04 11:53:42 +0700

    Update kmutnb.blade.php

--- resources/views/layouts/kmutnb.blade.php
+++ resources/views/layouts/kmutnb.blade.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>StudentAffairs</title>
 	<!-- Bootstrap core CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
 	<link href="{{ asset('kmutnb/css/bootstrap.min.css') }}" rel="stylesheet">
 	<!-- Full Calender CSS -->
@@ -445,6 +446,11 @@
     </div>
     <!--KF KODE WRAPPER WRAP END-->
 	<!--Bootstrap core JavaScript-->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
     <script type="text/javascript" src="https://counter2.stat.ovh/private/counter.js?c=29fg51hqetnket4mhnq72fpfgjzmgntt&down=async" async></script>
 <noscript><a href="https://www.freecounterstat.com" title="free counter"><img src="https://counter2.stat.ovh/private/freecounterstat.php?c=29fg51hqetnket4mhnq72fpfgjzmgntt" border="0" title="free counter" alt="free counter"></a></noscript>
 	<script src="{{ asset('kmutnb/js/jquery.js') }}"></script>
