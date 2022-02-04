983c934800815a572f6742330a5ec7853732b16f
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-02-04 11:53:17 +0700

    up

--- resources/views/layouts/kmutnb.blade.php
+++ resources/views/layouts/kmutnb.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Education</title>
+	<title>StudentAffairs</title>
 	<!-- Bootstrap core CSS -->
     
 	<link href="{{ asset('kmutnb/css/bootstrap.min.css') }}" rel="stylesheet">
